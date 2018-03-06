537c09a69b46d3e2019fd77b1cc3e49150c4f86a
kumarmanish11 <36592896+kumarmanish11@users.noreply.github.com>
2018-03-06 16:27:35 +0530

    Add files via upload

---
+++ pattern.java
@@ -0,0 +1,27 @@
+package Learn;
+
+public class pattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for (int i = 0; i < 6; i++) {
+			
+			for (int j = 6; j >=i;j--) {
+				System.out.print("$");
+				
+			}
+			System.out.println();
+		}
+		for (int i = 0; i < 6; i++) {
+			
+			for (int j = 0; j <=i;j++) {
+				System.out.print("$");
+				
+			}
+			System.out.println();
+		}
+
+	}
+
+}
